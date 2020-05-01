83deda18b9c4df5c05ffbca780d88744b6344f67
creepystaisy <creepystaisy@users.noreply.github.com>
2020-05-01 04:01:47 +0300

    Relise 0.0.0.1

---
+++ README.md
@@ -0,0 +1,18 @@
+
+## Создание контакта
+
+POST /demo/add
+
+name, email
+
+## Получить все контакты
+
+GET /demo/all
+
+## Найти контакт по ид
+
+GET /demo/find?id={id}
+
+## Отправить емаил контакту
+
+GET /demo/send?id={id}
---
+++ src/main/java/com/example/MailSender/mail/MyConstants.java
@@ -0,0 +1,11 @@
+package com.example.MailSender.mail;
+
+public class MyConstants {
+
+    // Replace with your email here:
+    public static final String MY_EMAIL = "creepystaisy97@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "89198237590aA1bbB";
+
+}
